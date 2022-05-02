318335691a3428b1cf64d7938ff7049b3267eabe
emarahch <emarah.charles19@bcmail.cuny.edu>
2022-05-02 19:27:51 -0400

    Initial Commit

---
+++ ArtAppy/ArtAppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAppyApp.swift
+//  ArtAppy
+//
+//  Created by Emarah Charles on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtAppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtAppy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArtAppy
+//
+//  Created by Emarah Charles on 5/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
